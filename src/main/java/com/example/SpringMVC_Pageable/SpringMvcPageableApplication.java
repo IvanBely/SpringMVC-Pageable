82490547bcb1de0e30a7cd52763963839d4b05e4
ivan <belyakov_ivan96@mail.ru>
2024-07-25 09:28:58 +0300

    init

---
+++ src/main/java/com/example/SpringMVC_Pageable/SpringMvcPageableApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringMVC_Pageable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcPageableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcPageableApplication.class, args);
+	}
+
+}
